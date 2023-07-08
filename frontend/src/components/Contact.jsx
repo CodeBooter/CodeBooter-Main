845996358f75fb7dcabe66835024c51e5fcf1d1d
Roshni Kumari01~ <roshanikumari06112004@gmail.com>
2023-07-08 14:57:55 +0530

    project done

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -1,30 +1,48 @@
 import React from 'react'
 
 const Contact = () => {
-    return (
-        <section className='h-60 p-20'>
-            <h5>Get In Touch</h5>
-
-            <div className="flex gap-20">
-                <div className="contact__options">
-
-                    <h4>Email</h4>
-                    <h5>codebooter@gmail.com</h5>
-                    <a href="mailto:nehak78800@gmail.com" target='_blank' rel="noreferrer">Send a message</a>
-
-                </div>
-
-                {/* end of contact options */}
-                <div>
-                <form className='flex flex-col'>
-                    <input type="text" name='name' placeholder='Your Full Name' required />
-                    <input type="email" name='email' placeholder='Your Email' required />
-                    <textarea>Enter</textarea>
-                </form>
-                </div>
+  return (
+     <div 
+     name='contact' 
+     className="w-full h-screen bg-gradient-to-b from-black to-gray-800 p-4 text-white"
+     >
+        <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
+            <div className="pb-8">
+                <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+                  Contact
+                </p>
+                <p className="py-6">Submit the form below to get in touch with us</p>
             </div>
-        </section>
-    )
-}
 
-export default Contact
+            <div className=" flex justify-center items-center">
+                <form action="https://getform.io/f/20e6c1b5-b76c-4c6c-8235-972d83a0a622" method="POST" className=" flex flex-col w-full md:w-1/2">
+                    <input 
+                     type="text" 
+                     name="name" 
+                     placeholder="Enter your Name"
+                     className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+                    />
+                    <input 
+                     type="text" 
+                     name="email" 
+                     placeholder="Enter your email"
+                     className="my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+                    />
+                    <textarea
+                     name="message"
+                     placeholder="Enter your message"
+                     rows="10"
+                     className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+                    ></textarea>
+
+                    <button className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">
+                     Let's Connect 
+                    </button>
+                </form>
+            </div>
+        </div>
+    </div>
+  );
+};
+
+export default Contact;
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,18 +1,79 @@
 import React from 'react'
 import { Link } from "react-router-dom"
+import { FaBars, FaTimes } from "react-icons/fa";
+import { Link } from 'react-scroll'
 
 const Navbar = () => {
+
+  const [nav, setNav] = useState(false);
+
+  const links = [
+    {
+      id: 1,
+      link: "home",
+    },
+    {
+      id: 2,
+      link: "about",
+    },
+    {
+      id: 3,
+      link: "services",
+    },
+    {
+      id: 4,
+      link: "projects",
+    },
+    {
+      id: 5,
+      link: "contact",
+    },
+  ];
+
   return (
-    <header className='flex justify-between h-10 fixed top-0 left-0 right-0'>
-      <Link to="/">CodeBooter</Link>
-      <nav >
-        <Link to="/about">About</Link>
-        <Link to='/internship'>Internship</Link>
-        <Link to='/accomplishment'>Accomplishment</Link>
+  <div className="flex justify-betwenn items-center w-full h-20 px-4 text-white bg-black fixed">
+      <div>
+        <h1 className="text-5xl font-signature ml-2">CodeBooter</h1>
+    </div>
 
-      </nav>
-</header>
-      )
-}
+    <ul className="hidden md:flex">
+        {links.map(({ id, link }) => (
+          <li 
+          key={id} 
+          className="px-4 cursor-pointer captalize font-medium text-gray-500 hover:scale-105 duration-200"
+          >
+            <Link to={link} smooth duration={500}>
+              {link}
+            </Link>
+          </li>
+          ))}
+    </ul>
 
-      export default Navbar
+    <div
+        onClick={() => setNav(!nav)}
+        className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden"
+        >
+          {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
+      </div>
+
+      {nav && (
+          <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
+          {links.map(({ id, link }) => ( 
+            <li key={id} className="px-4 cursor-pointer capitalize py-6 text-4xl"
+            >
+              <Link onClick={() => setNav(!nav)} 
+              to={link} 
+              smooth 
+              duration={500}
+              >
+                {link}
+              </Link>
+          </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+      export default Navbar;
