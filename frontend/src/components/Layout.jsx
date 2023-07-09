4ea099486c64082b92b18eabadb279d82c1b03bf
neha <nehak78800@gmail.com>
2023-07-09 22:54:17 +0530

    add navbar

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
-import Navbar from './Navbar'
+import {Outlet} from "react-router-dom"
+import Navbar from './Navbar.jsx'
+import Footer from './Footer.jsx'
+
+
 
 const Layout = () => {
   return (
+   
     <div>
-    <Navbar />
-    <Outlet/>
+     <Navbar />
+     <Outlet />
+     <Footer />
     </div>
   )
 }
